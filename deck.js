353457b694750bc398d9071abc31e3fab48db5c2
Charles King <bringking@gmail.com>
2018-11-01 10:39:36 -0700

    add profile and closing (#5)
    * add start
    
    * add agenda item

--- deck.js
+++ deck.js
@@ -4,10 +4,12 @@ import intro from './intro.mdx'
 import hooks from './hooks.mdx'
 import suspense from './suspense.mdx'
 import profiler from './profiler.mdx'
+import misc from './misc.mdx'
 
 export default [
   ...intro,
   ...hooks,
   ...suspense,
-  ...profiler
+  ...profiler,
+  ...misc
 ]
